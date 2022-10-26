acf426d7b1f44636b041bb5fff56ba6189b79ff6
alanchede <57455589+alanchede@users.noreply.github.com>
2022-10-26 13:11:41 +0200

    fin ej 2

--- block1-expression-evaluator/src/main/java/org/example/Main.java
+++ block1-expression-evaluator/src/main/java/org/example/Main.java
@@ -2,15 +2,20 @@ package org.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
+
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, ParseException {
-        String ruta = "expresiones.csv";
+        String ruta = "block1-expression-evaluator/expresiones.csv";
         Scanner sc = new Scanner(new File(ruta));
-
         while (sc.hasNext()) {
+
             String fila = sc.nextLine();
             System.out.println(comprobarExpresiones(fila));
         }
@@ -18,41 +23,74 @@ public class Main {
     }
 
 
-
-
-public static String comprobarExpresiones(String linea) throws ParseException {
+    public static String comprobarExpresiones(String linea) throws ParseException {
         String[] datos = linea.split(" ");
         String tipoDatos = "";
-        for(String dato:datos){
-            if(dato.contains("\"")){
+        for (String dato : datos) {
+            if (dato.contains("\"")) {
                 tipoDatos = "string";
-            }
-            else{
+            } else if (dato.matches("[0-9]{4}/[0-9]{2}/[0-9]{2}")) {
+                tipoDatos = "date";
+            } else {
                 tipoDatos = "num";
             }
-            //else{
-            //    tipoDatos = "date";
-            //}
         }
-        return linea + ": " + ejecutarExpresion(datos, tipoDatos);
+        return "La línea: " + linea + " da como resultado: " + ejecutarExpresion(datos, tipoDatos);
     }
 
     public static String ejecutarExpresion(String[] linea, String tipoDatos) throws ParseException {
-        String resultado = linea[0].replace("\"","");
+        String resultado = linea[0].replace("\"", "");
 
-        if(tipoDatos.equals("string")){
-            for (int i =0 ; i < linea.length; i++){
-
+        if (tipoDatos.equals("string")) {
+            for (int i = 0; i < linea.length; i++) {
+                if (i % 2 != 0) {
+                    switch (linea[i]) {
+                        case "+":
+                            resultado = resultado + " " + linea[i + 1].replace("\"", "");
+                            break;
+                        case "*":
+                            resultado = resultado + resultado;
+                            break;
+                    }
+                }
             }
         }
-        if(tipoDatos.equals("num")){
-            for (int i =0 ; i < linea.length; i++){
-
+        if (tipoDatos.equals("num")) {
+            for (int i = 0; i < linea.length; i++) {
+                if (i % 2 != 0) {
+                    switch (linea[i]) {
+                        case "+":
+                            resultado = String.valueOf(parseInt(resultado) + parseInt(linea[i + 1]));
+                            break;
+                        case "-":
+                            resultado = String.valueOf(parseInt(resultado) - parseInt(linea[i + 1]));
+                            break;
+                        case "*":
+                            resultado = String.valueOf(parseInt(resultado) * parseInt(linea[i + 1]));
+                            break;
+                        case "/":
+                            resultado = String.valueOf(parseInt(resultado) / parseInt(linea[i + 1]));
+                            break;
+                    }
+                }
             }
         }
-        if(tipoDatos.equals("date")){
-            for (int i =0 ; i < linea.length; i++){
-
+        if (tipoDatos.equals("date")) {
+            DateFormat fecha = new SimpleDateFormat("yyyy/MM/dd");
+            for (int i = 0; i < linea.length; i++) {
+                if (i % 2 != 0) {
+                    switch (linea[i]) {
+                        case "=":
+                            resultado = String.valueOf(fecha.parse(resultado).compareTo(fecha.parse(linea[i + 1])) == 0);
+                            break;
+                        case "<":
+                            resultado = String.valueOf(fecha.parse(resultado).compareTo(fecha.parse(linea[i + 1])) < 0);
+                            break;
+                        case ">":
+                            resultado = String.valueOf(fecha.parse(resultado).compareTo(fecha.parse(linea[i + 1])) > 0);
+                            break;
+                    }
+                }
             }
         }
 
